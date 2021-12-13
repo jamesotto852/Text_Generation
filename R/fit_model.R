7a2e1bf962ff614889d92bf61877c3213b37afd3
jamesotto852 <jamesotto852@gmail.com>
2021-12-13 20:14:06 +0000

    Include commented line implements lstm instead of simple rnn

--- R/fit_model.R
+++ R/fit_model.R
@@ -18,8 +18,8 @@ fit_model <- function(author, k, n_epochs) {
   # NN with embedding and RNN
   model <- keras_model_sequential() |>
     layer_embedding(input_dim = n_target_nodes, output_dim = 16) |>
+    # layer_lstm(units = 1000, activation = "tanh") |>
     layer_simple_rnn(units = 1000, activation = "relu") |>
-    # layer_dense(units = 100, activation = "relu") |>
     layer_dense(units = n_target_nodes, activation = "softmax")
 
   model |> compile(loss = "sparse_categorical_crossentropy", optimizer = "adam", metrics = "accuracy")
